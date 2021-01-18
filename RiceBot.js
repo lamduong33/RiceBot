a65eab9c0043e3ae9262178399a02b1238c5072f
lamduong33 <lamthachduong@gmail.com>
2021-01-18 15:09:19 -0500

    Added commands to RiceBot

--- RiceBot.js
+++ RiceBot.js
@@ -12,6 +12,22 @@ var fs = require("fs");
 const riceBot = new Discord.Client();
 riceBot.login(token);
 
+// Get list of tracks URLS
+let rawData = fs.readFileSync("usertracks.json");
+let userTracks = JSON.parse(rawData);
+
+// For denbugging and seeing the list of user tracks and associated users
+function getUserTracks()
+{
+	for (var i = 0; i < userTracks.userTracks.length; i++)
+	{
+		console.log(userTracks.userTracks[i].id);
+	}
+}
+
+// Get list of users who do not have welcome messages, such as bots
+let rawData2 = fs.readFileSync("noIntroUsers.json");
+
 // Listener and logging
 riceBot.once("ready", () =>
 {
@@ -37,8 +53,6 @@ riceBot.on("message", async message =>
     if (!message.content.startsWith(prefix)) return; 
 }); // assign message
 
-
-
 riceBot.on("voiceStateUpdate", (oldState, newState) =>
 {
     if ((newState.channelID !== null) && (oldState.channelID === null))
@@ -50,7 +64,8 @@ riceBot.on("voiceStateUpdate", (oldState, newState) =>
         voiceChannel.join().then(connection =>
         {
             let streamOptions = {seek: 0, volume: 1};
-            let stream = ytdl('https://www.youtube.com/watch?v=i8a3gjt_Ar0');
+            let defaultGreeting = ytdl('https://www.youtube.com/watch?v=i8a3gjt_Ar0');
+            let stream = defaultGreeting;
             const dispatcher = connection.play(stream, streamOptions);
 
             // Timeout for discord bot
@@ -68,4 +83,35 @@ riceBot.on("voiceStateUpdate", (oldState, newState) =>
     {
         console.log(newState.member.displayName + " left the chat");
     }
+});
+
+// COMMANDS FOR RICEBOTS
+riceBot.on("message", message =>
+{
+    // Format the command to lowercase
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	const args = message.content.slice(prefix.length).split(' ');
+	const command = args.shift().toLowerCase();
+
+    // Set track command. allows user to manually set the track for their intro
+	if (command === 'settrack')
+	{
+		let stream = args[0];
+        // Search through list of user tracks
+		for (var i = 0; i < userTracks.userTracks.length; i++)
+		{
+			if (userTracks.userTracks[i].id === message.author.id)
+			{
+				userTracks.userTracks[i].track = stream;
+				console.log(`${userTracks.userTracks[i].id}: ${userTracks.userTracks[i].track}`);
+                getInfo('https://www.youtube.com/watch?v=YQHsXMglC9A', function(err, info) {
+				    message.reply(`You've set your track to ${info.title}`);
+                });
+			}
+		}
+		fs.writeFile('usertracks.json', JSON.stringify(userTracks, null, 4), function (err,result)
+		{
+			if(err) console.log('error', err);
+		});
+	}
 });
