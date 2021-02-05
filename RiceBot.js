a9f1ac3e974287c03b261306559419c6ecea436e
lamduong33 <lamthachduong@gmail.com>
2021-02-05 16:11:13 -0500

    Added quality of life improvements. Ensure Links are valid. Will now play correctly. If the sound ends, will leave. If it's been 10 seconds, then it'll leave

--- RiceBot.js
+++ RiceBot.js
@@ -1,13 +1,16 @@
 // Dependencies listing
 const Discord = require("discord.js");
 const
-{
-    prefix,
-    token
-} = require("./config.json");
+    {
+        prefix,
+        token
+    } = require("./config.json");
 const ytdl = require("ytdl-core"); // For playing music on YT
 var fs = require("fs");
- 
+
+// WELCOME TO THE RICE FIELDS
+var defaultGreeting = 'https://www.youtube.com/watch?v=i8a3gjt_Ar0';
+
 // Login and authenticate BOT
 const riceBot = new Discord.Client();
 riceBot.login(token);
@@ -16,11 +19,9 @@ riceBot.login(token);
 let rawData = fs.readFileSync("usertracks.json");
 let userTracks = JSON.parse(rawData);
 
-// For denbugging and seeing the list of user tracks and associated users
-function GetUserTracks()
-{
-    for (var i = 0; i < userTracks.userTracks.length; i++)
-    {
+// For debugging and seeing the list of user tracks and associated users
+function GetUserTracks() {
+    for (var i = 0; i < userTracks.userTracks.length; i++) {
         console.log(userTracks.userTracks[i].id);
     }
 }
@@ -29,98 +30,106 @@ function GetUserTracks()
 let rawData2 = fs.readFileSync("noIntroUsers.json");
 
 // Listener and logging
-riceBot.once("ready", () =>
-{
+riceBot.once("ready", () => {
     console.log("RiceBot is ready!");
 });
-riceBot.once("reconnecting", () =>
-{
+riceBot.once("reconnecting", () => {
     console.log("RiceBot is reconnecting...");
 });
-riceBot.once("disconnect", () =>
-{
+riceBot.once("disconnect", () => {
     console.log("Ricebot is disconnected!!");
 });
-riceBot.once("error", () =>
-{
+riceBot.once("error", () => {
     console.log("ERROR!!!");
 });
 
 // Read commands from user messages
-riceBot.on("message", async message =>
-{
+riceBot.on("message", async message => {
     if (message.author.bot) return; // ignore message if it's from the bot
-    if (!message.content.startsWith(prefix)) return; 
+    if (!message.content.startsWith(prefix)) return;
 }); // assign message
 
 // Whenever a state is changed
-riceBot.on("voiceStateUpdate", (oldState, newState) =>
-{
-    if ((newState.channelID !== null) && (oldState.channelID === null))
-    {
+riceBot.on("voiceStateUpdate", (oldState, newState) => {
+    if ((newState.channelID !== null) && (oldState.channelID === null)) {
         console.log(oldState.member.displayName + " joined the chat");
         var voiceChannel = newState.channel; // channel user just joined
 
         // Bot joins channel
-        voiceChannel.join().then(connection =>
-        {
-            let streamOptions = {seek: 0, volume: 1};
-            let defaultGreeting = ytdl('https://www.youtube.com/watch?v=i8a3gjt_Ar0');
-            let stream = defaultGreeting;
+        voiceChannel.join().then(connection => {
+            let streamOptions = { seek: 0, volume: 1 };
+            let stream = ytdl(defaultGreeting);
+            let streamURL = "";
 
-            for (var i = 0; i < userTracks.userTracks.length; i++)
-            {
+            for (var i = 0; i < userTracks.userTracks.length; i++) {
                 // Check to see if the user is in the list of users
-                if (newState.member.id == userTracks.userTracks[i].id)
-                {
-                    stream = ytdl(userTracks.userTracks[i].track);
+                if (newState.member.id == userTracks.userTracks[i].id) {
+                    streamURL = userTracks.userTracks[i].track;
+                    stream = ytdl(streamURL);
+                    break;
                 }
             }
-            console.log("Playing %s", stream)
+            console.log("Playing %s", streamURL);
+
+            // Play the stream here
             const dispatcher = connection.play(stream, streamOptions);
 
-            // Timeout for discord bot
-            setTimeout(function()
-            {
-                dispatcher.on("finish", () => 
-                {
-                    console.log("Welcome for %s ended", newState.member.displayName);
-                    voiceChannel.leave();
-                });
+            // Leave after finish
+            dispatcher.on("finish", () => {
+                console.log("Welcome for %s ended", newState.member.displayName);
+                voiceChannel.leave();
+            });
+
+            setTimeout(function(){
+                console.log("Welcome for %s elapsed for 10 seconds. Ending.", newState.member.displayName);
                 voiceChannel.leave();
             }, 10000);
+
         }).catch(err => console.log(err));
     }
-    else if ((newState.channelID === null) && (oldState.channelID !== null))
-    {
+    else if ((newState.channelID === null) && (oldState.channelID !== null)) {
         console.log(newState.member.displayName + " left the chat");
     }
 });
 
+
 // COMMANDS FOR RICEBOTS
-riceBot.on("message", message =>
-{
+riceBot.on("message", message => {
     // Format the command to lowercase
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(' ');
     const command = args.shift().toLowerCase();
 
     // Set track command. allows user to manually set the track for their intro
-    if (command === 'settrack')
-    {
+    if (command === 'settrack') {
         let stream = args[0];
         // Search through list of user tracks
-        for (var i = 0; i < userTracks.userTracks.length; i++)
-        {
-            if (userTracks.userTracks[i].id === message.author.id)
-            {
+        for (var i = 0; i < userTracks.userTracks.length; i++) {
+
+            // If the user is in the list of valid users, usertracks.json
+            if (userTracks.userTracks[i].id === message.author.id) {
+
+                // If it's the default
+                if (stream === "default"){
+                    console.log("Set default message for %s", message.author.id);
+                    message.channel.send("You've set your welcome message to default");
+                    stream = defaultGreeting;
+                }
+
+                // If the track is not valid
+                if (ytdl.validateURL(stream) === false){
+                    console.log("Invalid %stream", stream);
+                    message.channel.send("That was an invalid link. Please try again.")
+                    break;
+                }
+
                 userTracks.userTracks[i].track = stream;
                 console.log(`${userTracks.userTracks[i].id}: ${userTracks.userTracks[i].track}`);
+                break;
             }
         }
-        fs.writeFile('usertracks.json', JSON.stringify(userTracks, null, 4), function (err, result)
-        {
-            if(err) console.log('error', err);
+        fs.writeFile('usertracks.json', JSON.stringify(userTracks, null, 4), function(err, result) {
+            if (err) console.log('error', err);
         });
     }
 });
