388f57e96370a1bd76f77d02553b1efe9ba9eeca
lamduong33 <lamthachduong@gmail.com>
2021-03-10 12:33:19 -0500

    Successfully added function to have specific channel voice clip

--- RiceBot.js
+++ RiceBot.js
@@ -20,6 +20,11 @@ let userDB = JSON.parse(rawUsersList);
 let botsListFile = fs.readFileSync("botslist.json");
 let bots = JSON.parse(botsListFile);
 
+// get server settings
+let serverSettingsFile = fs.readFileSync("ricebotsettings.json")
+let serverSettings = JSON.parse(serverSettingsFile)
+let serverAdminID = serverSettings.admin
+
 
 function inBotsList(memberID) {
   // check to see if the user is in list of bots
@@ -51,7 +56,10 @@ riceBot.on("message", async (message) => {
   if (!message.content.startsWith(prefix)) return;
 }); // assign message
 
-// Whenever a state is changed
+/* Where the magic happens. This allows the bot to listen on the state of the
+ * voice channel. Once it gets updated, we will then check the new state and see
+ * who joins, then play the track.
+ */
 riceBot.on("voiceStateUpdate", (oldState, newState) => {
   // Check if a new user has joined the channel due to channel's state change.
   if (
@@ -71,43 +79,32 @@ riceBot.on("voiceStateUpdate", (oldState, newState) => {
         let stream = ytdl(defaultGreeting);
         let streamURL = "";
 
-        debugger;
         for (var i = 0; i < userDB.users.length; i++) {
           let user = userDB.users[i]
           // Check to see if the user is in the list of users
           if (newState.member.id == user.id) {
             streamURL = user.tracks[0].track
             for (var j = 0; j < user.tracks.length; j++) {
-              track = user.tracks[i]
-              // Check current channelID to match with database ID
-              if (track.channelID == newState.channelID) {
+              track = user.tracks[j]
+              // Check current name to match with database ID
+              if (track.channelName == newState.channel.name) {
                 streamURL = track.track
               }
             }
             stream = ytdl(streamURL);
             break;
           }
-          else {
-            console.log("User isn't in list of users\n")
-          }
         }
-
-        console.log("Playing %s", streamURL);
 
         // Play the stream here
         const dispatcher = connection.play(stream, streamOptions);
 
         // Leave after finish
         dispatcher.on("finish", () => {
-          console.log("Welcome for %s ended", newState.member.displayName);
           voiceChannel.leave();
         });
 
         setTimeout(function() {
-          console.log(
-            "Welcome for %s elapsed for 10 seconds. Ending.",
-            newState.member.displayName
-          );
           voiceChannel.leave();
         }, 10000);
       })
@@ -124,52 +121,80 @@ riceBot.on("message", (message) => {
   const args = message.content.slice(prefix.length).split(" ");
   const command = args.shift().toLowerCase();
 
-  // Set track command. allows user to manually set the track for their intro
-  if (command === "settrack") {
-    setTrackCommand()
+  // Interpret different commands
+  if (command == "settrack") {
+    setTrackCommand(args, message)
   }
 });
 
 /* setTrackCommand:
  * -----------------------------------------------------------------------------
- * input: args -> Message
+ * input: args -> string[]
+ *        message -> Discord.Message
  *
- * This function will set the track for the user. It will parse the argument
+ * This function will set the DEFAULT track for user. It will parse the argument
  * passed into chat. Refer to the userDB.json file to understand the database
  * structure.
  */
-function setTrackCommand(args) {
-  let stream = args[0];
-  // Search through list of user tracks
-  for (var i = 0; i < userTracks.userTracks.length; i++) {
-    // If the user is in the list of valid users, usertracks.json
-    if (userTracks.userTracks[i].id === message.author.id) {
-      // If it's the default
-      if (stream === "default") {
-        console.log("Set default message for %s", message.author.id);
-        message.channel.send("You've set your welcome message to default");
-        stream = defaultGreeting;
-      }
+function setTrackCommand(args, message) {
+  let streamURL = args[0];
+  let channelName = "0"
 
+  // Handle different arguments
+  if (args.length <= 2) {
+    channelName = args[1]
+  } else {
+    message.channel.send("Please follow the format -> [URL] [Channels' Name]")
+  }
+
+  // Search through list of user tracks
+  for (var i = 0; i < userDB.users.length; i++) {
+
+    user = userDB.users[i]
+    userID = message.author.id
+
+    // If the user is in the list of valid users, userDB.json
+    if (user.id === userID) {
+
+      // If it's the default
+      if (streamURL === "default") {
+        console.log("Set default message for %s", userID);
+        message.channel.send("You've set your welcome message to default");
+        streamURL = defaultGreeting;
+      }
       // If the track is not valid
-      if (ytdl.validateURL(stream) === false) {
-        console.log("Invalid %stream", stream);
+      if (ytdl.validateURL(streamURL) === false) {
+        console.log("Invalid stream URL: %s", streamURL);
         message.channel.send("That was an invalid link. Please try again.");
         break;
       }
 
-      userTracks.userTracks[i].track = stream;
-      console.log(
-        `${userTracks.userTracks[i].id}: ${userTracks.userTracks[i].track}`
+      // Set the stream and write to JSON file
+      found = false
+      for (j = 0; j < user.tracks.length; j++) {
+        if (user.tracks[j].channelName === channelName){
+          user.tracks[j].track = streamURL
+          found = true
+        }
+      }
+      if (found === false) // If it's a new channel being inserted
+        user.tracks.push({"channelName" : channelName, "track": streamURL})
+
+      // Write to JSON file
+      fs.writeFile(
+        "userDB.json",
+        JSON.stringify(userDB, null, 4),
+        function(err, result) {
+          if (err) console.log("error", err);
+        }
       );
       break;
+    } else {
+      owner = message.member.guild.owner.displayName
+      message.channel.send("You are not in the list of users to use RiceBot." +
+        " Please contact " + owner)
+      break
     }
+
   }
-  fs.writeFile(
-    "usertracks.json",
-    JSON.stringify(userTracks, null, 4),
-    function(err, result) {
-      if (err) console.log("error", err);
-    }
-  );
 }
