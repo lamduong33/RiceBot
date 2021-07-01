2ff8089d4e8f4f3c9550cd1cb7f2e7cfdfd62bf9
lamduong33 <lamthachduong@gmail.com>
2021-07-01 18:23:04 -0400

    Finished the implementation for welcome intro.

--- RiceBot.py
+++ RiceBot.py
@@ -1,29 +1,66 @@
+import os
 import discord
 from discord.ext import commands
-import os
 import logging
-import pdb  # for debugging
+from discord.player import AudioSource
+
 from dotenv import load_dotenv  # --require=dotenv/config
 import youtube_dl
 import asyncio
 
-# ===================================BOT SETUP===================================
+# ===================================SETUP===================================
 load_dotenv()
-DISCORD_TOKEN = os.getenv("discord_token")
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents().all()
+command_prefix = "!"
 intents.members = True
-ricebot = commands.Bot(command_prefix=commands.when_mentioned_or("!"), intents=intents)
-defaultURL = "https://www.youtube.com/watch?v=HKtaHdA6IFc"
-
 # Logging/debugging purposes
 logging.basicConfig(level=logging.INFO)
+defaultURL = "https://www.youtube.com/watch?v=HKtaHdA6IFc"  # WELCOME
+
+# ===================================RICEBOT===================================
+class RiceBot(commands.Bot):
+    def __init__(self, t_prefix: str, t_intents: discord.Intents):
+        super().__init__(command_prefix=t_prefix, intents=t_intents)
+
+    async def on_ready(self):
+        print("Logged in as {0} ({0.id})".format(ricebot.user))
+        print("------")
+
+    async def on_voice_state_update(
+        self,
+        member: discord.Member,
+        before: discord.VoiceState,
+        after: discord.VoiceState,
+    ):
+        """Once the user joins the channel, play a track to them"""
+        if before.channel == None and after.channel != None:
+            print("{} joined!".format(member.display_name))
+
+            # Joins the channel
+            await after.channel.connect()
+
+            # Play the music
+            player = await YTDLSource.from_url(defaultURL, loop=self.loop)
+            duration = player.data["duration"]
+            member.guild.voice_client.play(
+                player, after=lambda e: print("Player error: %s" % e) if e else None
+            )
+            # Disconnect
+            await asyncio.sleep(duration)
+            await member.guild.voice_client.disconnect()
+        elif after.channel == None:
+            print("{} left!".format(member.display_name))
+
+
+ricebot = RiceBot(command_prefix, intents)
+
 
 # =====================================YTDL======================================
 youtube_dl.utils.bug_reports_message = lambda: ""
 ytdl_format_options = {
     "format": "bestaudio/best",
-    "extractaudio": True,
-    "audioformat": "mp3",
+    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
     "restrictfilenames": True,
     "noplaylist": True,
     "nocheckcertificate": True,
@@ -37,8 +74,9 @@ ytdl_format_options = {
 ffmpeg_options = {"options": "-vn"}
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
+
 class YTDLSource(discord.PCMVolumeTransformer):
-    """ Class for the YTDL Source, such as a video"""
+    """Class for the YTDL Source, such as a video"""
 
     def __init__(self, source, *, data, volume=0.5):
         super().__init__(source, volume)
@@ -48,97 +86,103 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
     @classmethod
     async def from_url(cls, url, *, loop=None, stream=False):
-        """Function to play music"""
         loop = loop or asyncio.get_event_loop()
         data = await loop.run_in_executor(
             None, lambda: ytdl.extract_info(url, download=not stream)
         )
+
         if "entries" in data:
             # take first item from a playlist
             data = data["entries"][0]
-        filename = data["title"] if stream else ytdl.prepare_filename(data)
-        return filename
+
+        filename = data["url"] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
 # ======================================MUSIC======================================
-
-
 class Music(commands.Cog):
-    """Music class inherits from Cog. A cog is just a collection of commands and
-    event listeners."""
+    """Music cog taken from DiscordPY documentation"""
+
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def join(self, ctx: commands.Context, *, channel: discord.VoiceChannel):
+    async def join(self, ctx, *, channel: discord.VoiceChannel):
         """Joins a voice channel"""
+
         if ctx.voice_client is not None:
             return await ctx.voice_client.move_to(channel)
+
         await channel.connect()
+
+    @commands.command()
+    async def play(self, ctx, *, query):
+        """Plays a file from the local filesystem"""
+
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        ctx.voice_client.play(
+            source, after=lambda e: print("Player error: %s" % e) if e else None
+        )
+
+        await ctx.send("Now playing: {}".format(query))
 
     @commands.command()
     async def yt(self, ctx, *, url):
         """Plays from a url (almost anything youtube_dl supports)"""
+
         async with ctx.typing():
             player = await YTDLSource.from_url(url, loop=self.bot.loop)
             ctx.voice_client.play(
                 player, after=lambda e: print("Player error: %s" % e) if e else None
             )
+
         await ctx.send("Now playing: {}".format(player.title))
 
     @commands.command()
     async def stream(self, ctx, *, url):
         """Streams from a url (same as yt, but doesn't predownload)"""
+
         async with ctx.typing():
             player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
             ctx.voice_client.play(
                 player, after=lambda e: print("Player error: %s" % e) if e else None
             )
+
         await ctx.send("Now playing: {}".format(player.title))
 
     @commands.command()
     async def volume(self, ctx, volume: int):
         """Changes the player's volume"""
+
         if ctx.voice_client is None:
             return await ctx.send("Not connected to a voice channel.")
+
         ctx.voice_client.source.volume = volume / 100
         await ctx.send("Changed volume to {}%".format(volume))
 
     @commands.command()
     async def stop(self, ctx):
         """Stops and disconnects the bot from voice"""
+
         await ctx.voice_client.disconnect()
 
+    @play.before_invoke
+    @yt.before_invoke
+    @stream.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You are not connected to a voice channel.")
+                raise commands.CommandError("Author not connected to a voice channel.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
 
-# ======================================RICEBOT======================================
-@ricebot.event
-async def on_ready():
-    print("Logged in as {0} ({0.id})".format(ricebot.user))
-    print("------")
 
-ricebot.add_cog(Music(ricebot)) # Add music cog
+# ======================================MAIN======================================
 
-@ricebot.event
-async def on_voice_state_update(
-    member: discord.Member, before: discord.VoiceState, after: discord.VoiceState
-):
-    """Once the user joins the channel, play a track to them"""
-    if before.channel == None and after.channel != None:
-        print("{} joined!".format(member.display_name))
-
-        # Joins the channel
-        await after.channel.connect()
-
-        # Play the music
-        # TODO: voice_client is null?
-        player = await YTDLSource.from_url(defaultURL, loop=ricebot.loop)
-        member.guild.voice_client.play(
-            player, after=lambda e: print("Player error: %s" % e) if e else None
-        )
-
-        # Disconnect
-    elif after.channel == None:
-        print("{} left!".format(member.display_name))
-
+ricebot.add_cog(Music(ricebot))
 if __name__ == "__main__":
     ricebot.run(DISCORD_TOKEN)
+    ricebot.run("NTY0MTYzODI4MDQxMTIxNzk1.XKj4gg.5JuYZiftXpKaU3SNcUZEKEvbIpM")
