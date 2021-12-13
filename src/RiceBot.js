ac091af97fd3a86fc9ba45e4a6add4905a806116
Lam T Duong <lamthachduong@gmail.com>
2021-12-13 10:23:28 -0500

    Deleted json folder. This should be private.

--- src/RiceBot.js
+++ src/RiceBot.js
@@ -2,7 +2,7 @@
 
 // Dependencies listing
 const { Discord, Intents } = require("discord.js");
-const { prefix, token } = require("../json/config.json");
+const { prefix, token } = require("../userSettings/config.json");
 const ytdl = require("ytdl-core"); // For playing music on YT
 var fs = require("fs");
 const { userInfo } = require("os");
@@ -19,15 +19,15 @@ const riceBot = new Discord.Client(
 riceBot.login(token);
 
 // Get list of tracks URLS
-let rawUsersList = fs.readFileSync("json/userDB.json");
+let rawUsersList = fs.readFileSync("userSettings/userDB.json");
 let userDB = JSON.parse(rawUsersList);
 
 // get list of users who do not have welcome messages, such as bots
-let suppressedUsersList = fs.readFileSync("json/suppressedUsers.json");
+let suppressedUsersList = fs.readFileSync("userSettings/suppressedUsers.json");
 let suppressedUsers = JSON.parse(suppressedUsersList);
 
 // get server settings
-let serverSettingsFile = fs.readFileSync("json/ricebotsettings.json");
+let serverSettingsFile = fs.readFileSync("userSettings/ricebotsettings.json");
 let serverSettings = JSON.parse(serverSettingsFile);
 let serverAdminID = serverSettings.admin;
 
