a051796560cd28c0f0c98e53c8c71960d3c6b3c2
Lam Duong <lamthachduong@gmail.com>
2024-03-14 21:34:34 -0400

    feat: Inference script for local llama

--- inference.sh
+++ inference.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env sh
 
+# No arguments, then exit
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <argument>"
+    exit 1
+fi
+
+CONTEXT_SIZE=512
+MODEL=capybarahermes-2.5-mistral-7b.Q4_K_M.gguf
+PROMPT=$1 # prompt is whatever that's passed via argument of the script
+TEMPERATURE=0.3
+
 # Assuming that llama.cpp's "main" binary is called "llama" here.
 # Any model is in .gguf extension
-./llama -m capybarahermes-2.5-mistral-7b.Q4_K_M.gguf --color -c 32768 --temp 0.7 --repeat_penalty 1.1 -n -1 -p "<|im_start|>system\n{system_message}<|im_end|>\n<|im_start|>user\n{prompt}<|im_end|>\n<|im_start|>assistant"
+./llama -m $MODEL -c $CONTEXT_SIZE --temp $TEMPERATURE \
+    --repeat_penalty 1.1 -n -1 -p "$PROMPT" \
+    2>/dev/null
