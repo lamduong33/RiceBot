df7ed71e205dfdf3d7492337ade1695564f48d7e
Lam Duong <lamthachduong@gmail.com>
2024-04-01 10:08:46 -0400

    feat: New startup sequence.

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+./server -m capybarahermes-2.5-mistral-7b.Q4_K_M.gguf &
+./target/debug/rice_bot                                                                                                                                                                                    10:06:24
