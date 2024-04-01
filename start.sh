d2a3e5075a25cc30b4d2f0070d7adfea044d7e73
Lam Duong <lamthachduong@gmail.com>
2024-04-01 13:05:34 -0400

    fix: Forces bash on start script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 ./server -m capybarahermes-2.5-mistral-7b.Q4_K_M.gguf &
 ./target/debug/rice_bot                                                                                                                                                                                    10:06:24
