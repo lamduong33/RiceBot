c1ade8863445349ede673bc5d5f293d6a2a11662
Lam Duong <lamthachduong@gmail.com>
2024-06-16 09:44:29 -0400

    feat: start script now requires token file as arg

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Check if an argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <model>"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <token_file> <model>"
     exit 1
 fi
 
 # Accessing the argument
-MODEL=$1
+TOKEN_FILE=$1
+MODEL=$2
 
 ./server -m "$MODEL" &
-./target/debug/rice_bot                                                                                                                                                                                    10:06:24
+./target/debug/rice_bot "$TOKEN_FILE" "$MODEL"
